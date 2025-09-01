7e98353da1adf7e1240ece6290db7833fa9ec74d
Paul Seale <paul-seale@cox.net>
2025-08-31 17:17:42 -0700

    commit

--- src/composables/useDeep.ts
+++ src/composables/useDeep.ts
@@ -4,10 +4,8 @@ type DeepValue = number | boolean | undefined
 type DeepOption = { name: string; value: DeepValue }
 
 const deepOptions: DeepOption[] = [
-  { name: 'default', value: undefined },
-  { name: 'false', value: false },
+  { name: 'false /* default */', value: false },
   { name: 'true', value: true },
-  { name: '0', value: 0 },
   { name: '1', value: 1 },
   { name: '2', value: 2 },
   { name: '3', value: 3 },
