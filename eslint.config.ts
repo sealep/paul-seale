41b1e68b1b0f6078f7e7e1b56d69d5969b774fac
Paul Seale <paul-seale@cox.net>
2025-09-19 15:11:18 -0700

    commit

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,8 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
